3052b89b9fc895aaec48696e87fef98596cab4a6
wickyhien18 <giaphien1008@gmail.com>
2026-04-17 12:31:09 +0700

    Fix syntax errors and cleanly resolve leftover conflict markers

--- model/deck.py
+++ model/deck.py
@@ -1,7 +1,4 @@
 import random
-<<<<<<< HEAD
-from model.card import Card
-=======
 import os
 import pygame
 from .card import Card
@@ -9,59 +6,10 @@ from pathlib import Path
 
 BASE_PATH = Path(__file__).parent.parent
 IMG_DIR = os.path.join(BASE_PATH, 'data', 'images')
->>>>>>> wicky
 
 class Deck:
     def __init__(self):
         self.cards = []
-<<<<<<< HEAD
-        self.build()
-        self.shuffle()
-        
-    def build(self):
-        self.cards = []
-        colors = ["red", "green", "blue", "yellow"]
-        
-        for color in colors:
-            # 0
-            self.cards.append(Card(color, "0", f"{color}_0.png"))
-            
-            # 1-9 (2 copies each)
-            for val in range(1, 10):
-                self.cards.append(Card(color, str(val), f"{color}_{val}.png"))
-                self.cards.append(Card(color, str(val), f"{color}_{val}.png"))
-                
-            # Action cards (+2, skip, reverse) (2 copies each)
-            self.cards.append(Card(color, "skip", f"{color}_skip.png"))
-            self.cards.append(Card(color, "skip", f"{color}_skip.png"))
-            self.cards.append(Card(color, "reverse", f"{color}_reverse.png"))
-            self.cards.append(Card(color, "reverse", f"{color}_reverse.png"))
-            self.cards.append(Card(color, "+2", f"{color}_+2.png"))
-            self.cards.append(Card(color, "+2", f"{color}_+2.png"))
-            
-        # Wild cards (+4, wildcard) (4 copies each)
-        for _ in range(4):
-            self.cards.append(Card("black", "wildcard", "black_wildcard.png"))
-            self.cards.append(Card("black", "+4", "black_+4.png"))
-            
-    def shuffle(self):
-        random.shuffle(self.cards)
-        
-    def draw(self):
-        if len(self.cards) > 0:
-            return self.cards.pop()
-        return None
-        
-    def recycle(self, discard_pile):
-        # Giữ lại lá bài trên cùng
-        top_card = discard_pile.pop()
-        
-        # Những lá bài còn lại dùng để xáo bài
-        self.cards.extend(discard_pile)
-        discard_pile.clear()
-        discard_pile.append(top_card)
-        self.shuffle()
-=======
         self.discard_pile = []
         self.build()
 
@@ -121,4 +69,3 @@ class Deck:
             img = pygame.image.load(path).convert_alpha()
             return pygame.transform.scale(img, (80, 120))
         return None
->>>>>>> wicky
